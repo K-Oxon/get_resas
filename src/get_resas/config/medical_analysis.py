f3e49a57c72d3b1109bd106b97fae45c6f4abb8f
K-Oxon <ko1011qfp@gmail.com>
2024-07-18 08:44:21 +0900

    add 医療地域分析

--- src/get_resas/config/medical_analysis.py
+++ src/get_resas/config/medical_analysis.py
@@ -1,4 +1,5 @@
 from get_resas.utils.base_models import BaseRequestModel, BaseResponseModel
+from get_resas.utils.get_pref_code import get_pref_code
 from pydantic import BaseModel, Field
 
 """
@@ -11,10 +12,10 @@ class MedicalAnalysisParams(BaseModel):
     dispType: int | None = Field(
         None, description="1: 実数表示（デフォルト）, 2: 人口10万人あたり"
     )
-    sort: int | None = Field(None, description="1: 降順, 2: 昇順")
+    sort: int | None = Field(None, description="1: 降順, 2: コード順")
     matter2: int = Field(..., description="データ種別 （102、103、201～208）")
-    broadCategoryCode: int = Field(..., description="大分類コード matter2に対応させる")
-    middleCategoryCode: int = Field(..., description="中分類コード matter2に対応させる")
+    broadCategoryCode: str = Field(..., description="大分類コード matter2に対応させる")
+    middleCategoryCode: str = Field(..., description="中分類コード matter2に対応させる")
     prefCode: str = Field(..., description="都道府県コード")
     cityCode: str = Field(
         ..., description="市区町村コード 「すべての市区町村」を選択する場合は「-」"
@@ -35,8 +36,15 @@ class MedicalAnalysisRequest(BaseRequestModel):
     params: MedicalAnalysisParams
 
     @classmethod
-    def generate_req_model_list(cls, matter2: int) -> list[MedicalAnalysisRequest]:
-        """市町村単位のリクエストを生成"""
+    def generate_req_model_list(cls, matter2: int):
+        """市町村単位のリクエストを生成（市町村単位）
+        市町村単位の場合はprefCodeに入れた都道府県ごとに取得する必要がある
+        （市町村コードは何を入れても都道府県が優先され、都道府県配下の市町村全て出力される）
+
+        都道府県・二次医療圏の場合は何かを入れてさえいれば全部出力される
+
+        イミフ仕様
+        """
         match matter2:
             # matter2が201-204の場合 -> 2002-2020年（3年毎）
             case 201 | 202 | 204:
@@ -45,13 +53,9 @@ class MedicalAnalysisRequest(BaseRequestModel):
             case 203:
                 year_tuple = (2002, 2005, 2008, 2011, 2014, 2017, 2020)
                 broad_category_code = "-"
-            # 205,206,208の場合 -> 2000-2020年（2年毎）
+            # 205の場合 -> 2008-2020年（2年毎）
             case 205:
                 year_tuple = (
-                    2000,
-                    2002,
-                    2004,
-                    2006,
                     2008,
                     2010,
                     2012,
@@ -61,6 +65,7 @@ class MedicalAnalysisRequest(BaseRequestModel):
                     2020,
                 )
                 broad_category_code = "00"
+            # 206,208の場合 -> 2000-2020年（2年毎）
             case 206 | 208:
                 year_tuple = (
                     2000,
@@ -78,26 +83,28 @@ class MedicalAnalysisRequest(BaseRequestModel):
                 broad_category_code = "-"
             case _:
                 raise ValueError("matter2が不正です")
+        pref_code_list = get_pref_code()
         disp_type_tuple = (1, 2)
         req_model_list = []
-        # 年 × 表示タイプごとにリクエストモデルを生成
+        # 年 × 47都道府県 × 表示タイプごとにリクエストモデルを生成
         for year in year_tuple:
-            for disp_type in disp_type_tuple:
-                req_model_list.append(
-                    cls(
-                        params=MedicalAnalysisParams(
-                            year=year,
-                            dispType=disp_type,
-                            # sort=1,
-                            matter2=matter2,
-                            broadCategoryCode=broad_category_code,
-                            middleCategoryCode="-",
-                            prefCode="1",  # 適当な値でOK
-                            cityCode="01100",  # 適当な値でOK
-                            secondaryMedicalCode="-",
+            for pref_code in pref_code_list:
+                for disp_type in disp_type_tuple:
+                    req_model_list.append(
+                        cls(
+                            params=MedicalAnalysisParams(
+                                year=year,
+                                dispType=disp_type,
+                                sort=2,  # なぜか必要
+                                matter2=matter2,
+                                broadCategoryCode=broad_category_code,
+                                middleCategoryCode="-",
+                                prefCode=str(pref_code),
+                                cityCode="01100",  # 適当な値でOK
+                                secondaryMedicalCode="-",
+                            )
                         )
                     )
-                )
         return req_model_list
 
 
@@ -120,10 +127,12 @@ class Result(BaseModel):
     broadCategoryCode: str
     middleCategoryCode: str
     year: str
-    prefecture_cd: str
-    municipality_cd: str
-    secondary_medical_cd: str
-    code: str
+    prefecture_cd: str = Field(..., description="対象の都道府県コード")
+    municipality_cd: str = Field(..., description="対象の市区町村コード")
+    secondary_medical_cd: str = Field(..., description="対象の二次医療圏コード")
+    code: str = Field(
+        ..., description="多分 有効な 都道府県|市区町村|二次医療圏 のコード"
+    )
     data: list[DataItem]
 
 
---
+++ src/get_resas/pipelines/medical_analysis_pipeline.py
@@ -0,0 +1,67 @@
+import dlt
+
+from get_resas.api_client import RESASAPIClient
+from get_resas.config.medical_analysis import (
+    MedicalAnalysisRequest,
+    MedicalAnalysisResponse,
+)
+from get_resas.utils.common_config import API_KEY
+from get_resas.utils.logger import get_my_logger
+
+logger = get_my_logger(__name__)
+
+
+@dlt.resource(
+    name="resas_medical_analysis",
+    table_name="resas_medical_analysis",
+    primary_key=[
+        "disp_type",
+        "matter2",
+        "broad_category_code",
+        "middle_category_code",
+        "year",
+        "prefecture_cd",
+        "municipality_cd",
+        "secondary_medical_cd",
+    ],
+    write_disposition="merge",
+)
+def get_medical_analysis_job(matter2: int):
+    api_client = RESASAPIClient(api_key=API_KEY)
+    req_model_list = MedicalAnalysisRequest.generate_req_model_list(matter2=matter2)
+    response = api_client.fetch_iter(
+        request_models=req_model_list,
+        with_params=False,  # responseに含まれるので不要
+        response_model=MedicalAnalysisResponse,
+    )
+    yield response
+
+
+def get_medical_analysis_pipeline():
+    pipeline = dlt.pipeline(
+        pipeline_name="medical_analysis",
+        destination="bigquery",
+        # dataset_name=dlt.config.value,
+        dataset_name="dl_localgov_kpi_database",
+        export_schema_path="src/get_resas/dlt_schemas/export",
+    )
+    load_info = pipeline.run(
+        [
+            get_medical_analysis_job(matter2=201),
+            get_medical_analysis_job(matter2=202),
+            get_medical_analysis_job(matter2=203),
+            # get_medical_analysis_job(matter2=204),
+            # get_medical_analysis_job(matter2=205),
+            # get_medical_analysis_job(matter2=206),
+            # get_medical_analysis_job(matter2=208),
+        ]
+    )
+    logger.info(load_info)
+    logger.info(f"loaded row counts: {pipeline.last_trace.last_normalize_info}")
+    return load_info
+
+
+if __name__ == "__main__":
+    get_medical_analysis_pipeline()
+    # data = get_medical_analysis_job()
+    # print(list(data))
